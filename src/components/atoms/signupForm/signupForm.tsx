26c13dcd5f8eb4bd6795e52ba3a3c87ee9de417a
Hemant Jadhav <hemant.jadhav@navgrahaa.local>
2024-09-11 17:48:47 +0530

    signup form added

--- src/components/atoms/signupForm/signupForm.tsx
+++ src/components/atoms/signupForm/signupForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { doCredentialLogin } from "@/app/actions";
-import SocialLogins from "@/components/molecules/SocialLogins";
 import { allRoutes } from "@/constants/allRoutes";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -31,11 +30,44 @@ const SignupForm = () => {
       <div className="text-xl text-red-500 dark:text-white text-center">
         {error.replace("Error:", "")}
       </div>
-      <SocialLogins />
-      <form onSubmit={onSubmit} className="space-y-6 relative" action="#">
-        <h5 className="text-xl -mb-5 text-center font-medium text-gray-900 dark:text-white">
-          OR
+      <form onSubmit={onSubmit} className="space-y-6" action="#">
+        <h5 className="text-xl text-center font-medium text-gray-900 dark:text-white">
+          Sign up
         </h5>
+        <div className="flex items-center justify-center gap-4">
+          <div>
+            <label
+              htmlFor="firstName"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              First name
+            </label>
+            <input
+              type="firstName"
+              name="firstName"
+              id="firstName"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="First name"
+              required
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="lastName"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Last name
+            </label>
+            <input
+              type="lastName"
+              name="lastName"
+              id="lastName"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="Last name"
+              required
+            />
+          </div>
+        </div>
         <div>
           <label
             htmlFor="email"
@@ -72,7 +104,7 @@ const SignupForm = () => {
           type="submit"
           className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
-          Login
+          Signup
         </button>
         <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
           <div className="mt-2 text-sm font-medium text-gray-500 dark:text-gray-300">
