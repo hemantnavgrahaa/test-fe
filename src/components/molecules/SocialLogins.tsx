b7aec470a8318e361e26a9a0d01b6fc05afc74b7
Hemant Jadhav <hemant.jadhav@navgrahaa.local>
2024-09-11 14:37:26 +0530

    login improved

--- src/components/molecules/SocialLogins.tsx
+++ src/components/molecules/SocialLogins.tsx
@@ -1,15 +1,18 @@
 import { doSocialLogin } from "@/app/actions";
+import Image from "next/image";
+import googleSvg from "@/images/google.svg";
 
 const SocialLogins = () => {
   return (
     <form className="text-center mb-3" action={doSocialLogin}>
       <button
-        className="bg-orange-600 text-white px-4 py-2 rounded-md m-1 text-base"
+        className="bg-slate-200 dark:bg-white w-full text-black px-4 py-2 rounded-md m-1 text-base flex items-center justify-center gap-2"
         type="submit"
         name="action"
         value="google"
       >
-        Sign In With Google
+        <Image src={googleSvg} width={25} height={25} alt="google" /> Log In
+        <p>With Google</p>
       </button>
     </form>
   );
