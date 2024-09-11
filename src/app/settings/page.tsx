f0db1f5b6668ae1c77c36fc7b25120449142d0cf
Hemant Jadhav <hemant.jadhav@navgrahaa.local>
2024-09-11 17:11:44 +0530

    settings changes

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -6,8 +6,11 @@ import BNavigationTwo from "@/components/atoms/bottomNavigation/bNavigationTwo";
 const Settings = () => {
   return (
     <>
+      <div className="flex items-center justify-center min-h-screen">
+        <p className="text-4xl dark:text-white">This is setting page</p>
+      </div>
       <BNavigationTwo />
-      <Card />
+      {/* <Card /> */}
     </>
   );
 };
